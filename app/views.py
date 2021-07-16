a1e9f42320068eaab21e8c28d608925e2899e85e
Marat Valeev <valeeva.la.1986@gmail.com>
2021-07-16 18:04:58 +0500

    manual paginator

--- app/views.py
+++ app/views.py
@@ -6,46 +6,12 @@ from django.http import HttpResponse
 
 CONTEXT = [str(i) for i in range(1000)]
 
-# def info_view(request, year, month, day):
-#     # none_result = 'Данных по пользователю нет'
-#     # data = {
-#     #     'Ivan': 'Начальник отдела',
-#     #     'Alex': 'Руководитель службы разработки',
-#     #     'Не определен': 'Нет данных'
-#     # }
-#     #
-#     # name = request.GET.get('name', 'Не определен')
-#     # return HttpResponse(f'Пользователь {name} - {data.get(name, none_result)}')
-#     #
-#     # # admin_email = settings.ADMIN_EMAIL
-#     # # return HttpResponse(f'Почта админа - {admin_email}')
-#
-#     date = datetime.datetime(year, month, day)
-#
-#     return HttpResponse(f'{date}')
-
-
-# def info_view(request, value):
-#     print('===>', type(value))
-#
-#     return HttpResponse(f'{value}')
 
 def info_view(request):
-    class User:
-        def __init__(self, name, age):
-            self.name = name
-            self.age = age
+    page = int(request.GET.get('page', 0))
+    elements_per_page = 10
 
-        def info(self):
-            return f'Пользователь {self.name} с возрастом {self.age}'
+    context = CONTEXT[page * elements_per_page: elements_per_page * (page + 1)]
 
-    user_1 = User('Ivan', '25')
-    user_2 = User('Alex', '30')
-
-    context = {
-        'info': 'Данные о пользователях',
-        'users': [user_1, user_2]
-    }
-
-    return render(request, 'app/templates/app/index.html', context)
+    return HttpResponse('<br>'.join(context))
 
