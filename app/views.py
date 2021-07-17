a177d77357aabd7a6f866b04e9d03c1bba9a8977
Marat Valeev <valeeva.la.1986@gmail.com>
2021-07-17 14:14:42 +0500

    django JsonResponse

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 import datetime
 from django.shortcuts import render
 from django.core.paginator import Paginator
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 CONTEXT = [str(i) for i in range(1000)]
@@ -15,7 +15,20 @@ def info_view(request):
     paginator = Paginator(CONTEXT, elements_per_page)
     page_ = paginator.get_page(page)
 
-    context = page_.object_list
+    # context = page_.object_list
 
-    return HttpResponse('<br>'.join(context))
+    # if page_.has_next():
+    #     n = page_.next_page_number()
+    #     print(f'След.страница - {n}')
+    #
+    # return HttpResponse('<br>'.join(context))
 
+    context = {
+        'data': page_.object_list,
+        'page': page_.number,
+        'next': page_.next_page_number() if page_.has_next() else paginator.num_pages,
+        'prev': page_.previous_page_number() if page_.has_previous() else 1,
+        'pages': paginator.num_pages
+    }
+
+    return JsonResponse(context)
